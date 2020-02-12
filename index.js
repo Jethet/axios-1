61c194b9eaa2ef376eae57741718012bd1e6daf5
Jethet <henriette.hettinga@gmail.com>
2020-02-12 11:44:28 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const urlString = 'https://restcountries.eu/rest/v2/name/spain';
+const baseURL = 'https://restcountries.eu/rest/v2/name/'; // all requests go to this url
+
+const countryList = document.querySelector('#country-list');
+const button = document.querySelector('#the-button');
+
+// axios must be in css as script, to use it at the frontend
+
+
+function getCountryByInfo(countryName) {  // If a letter is entered, the user gets a list
+    axios.get(`${baseURL}{$countryName}`) 
+    .then( (responseFromAPI) => {
+        const countries = responseFromAPI.data;
+
+        let htmlString = '';
+
+        countries.forEach ((countryObj) => {  // object details can be found in console
+            htmlString += `
+            <h2> ${coutryObj.name} </h2>
+            <p> ${countryObj.capital} <p>
+            <br>`;
+        })
+        countryList.innerHTL = htmlString;
+})
+    .catch (err => console.log(err))
+}
+
+const getCountryByName = axios.create(  // get request is put into a variable; this is called axios service
+     {baseURL: 'https://restcountries.eu/rest/v2/name/',
+      method: get
+    });
+
+
+function getCountryByInfo2(countryName) {
+    getCountryByName(countryName)
+    .then(responseFromAPI => {
+      const countries = responseFromAPI.data;
+
+      let htmlString = "";
+
+      countries.forEach(countryObj => {
+        htmlString += `
+        <h2> ${countryObj.name} </h2>
+        <p> ${countryObj.capital}</p>
+        <br>
+      `;
+      });
+
+      countryList.innerHTML = htmlString;
+    })
+    .catch(err => console.log(err));
+}
+
+
+
+
+button.addEventListener('click', () => {
+    const country = document.getElementById('the-input').value;
+    
+    getCountryInfo(country);
+});
+
